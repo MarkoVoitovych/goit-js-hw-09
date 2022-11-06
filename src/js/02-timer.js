55118ef6174dc476b5993ccfd5190d85c2630d61
MarkoVoitovych <searchangeos@gmail.com>
2022-11-06 16:17:39 +0200

    second-task-done

---
+++ src/js/02-timer.js
@@ -0,0 +1,95 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refs = {
+    calendarEl: document.querySelector('[id = "datetime-picker"]'),
+    startTimerBtnEL: document.querySelector('[data-start]'),
+    timerWrapEl: document.querySelector('.timer'),
+    timerFieldEls: document.querySelectorAll('.field'),
+    daysEl: document.querySelector('[data-days]'),
+    hoursEl: document.querySelector('[data-hours]'),
+    minutesEl: document.querySelector('[data-minutes]'),
+    secondsEl: document.querySelector('[data-seconds]'),
+};
+
+let saleEndDate = null;
+let timerId = null;
+
+startBtnDisabled();
+
+refs.startTimerBtnEL.addEventListener('click', () => {
+    onStartTimer();
+    clearInterval(timerId);
+    timerId = setInterval(onStartTimer, 1000);
+    startBtnDisabled();
+});
+
+flatpickr(refs.calendarEl, {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+        if (selectedDates[0] < Date.now()) {
+            startBtnDisabled();
+            notifyFailure();
+            return '';
+        }
+        startBtnAnabled();
+        saleEndDate = selectedDates[0];
+        clearInterval(timerId);
+        refs.daysEl.textContent = '00';
+        refs.hoursEl.textContent = '00';
+        refs.minutesEl.textContent = '00';
+        refs.secondsEl.textContent = '00';
+    },
+});
+
+function onStartTimer() {
+    const { days, hours, minutes, seconds } = convertMs(saleEndDate - Date.now());
+    if (seconds < 0) {
+        startBtnAnabled();
+        return '';
+    }
+    refs.daysEl.textContent = addLeadingZero(days);
+    refs.hoursEl.textContent = addLeadingZero(hours);
+    refs.minutesEl.textContent = addLeadingZero(minutes);
+    refs.secondsEl.textContent = addLeadingZero(seconds);
+}
+
+function addLeadingZero(value) {
+    return String(value).padStart(2, 0);
+}
+
+function convertMs(ms) {
+
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    const days = Math.floor(ms / day);
+    const hours = Math.floor((ms % day) / hour);
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+    return { days, hours, minutes, seconds };
+}
+
+function notifyFailure() {
+    Notify.failure('Please choose a date in the furute', {
+        timeout: 2000,
+        clickToClose: true,
+        pauseOnHover: false,
+        showOnlyTheLastOne: true,
+    });
+}
+
+function startBtnDisabled() {
+    refs.startTimerBtnEL.setAttribute('disabled', 'true');
+}
+
+function startBtnAnabled() {
+    refs.startTimerBtnEL.removeAttribute('disabled');
+}
